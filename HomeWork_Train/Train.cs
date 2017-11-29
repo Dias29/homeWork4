691031090867784a2cfd01e6f80dca6666551b85
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-11-29 22:19:37 +0600

    first

---
+++ HomeWork_Train/Train.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Train
+{
+    class Train
+    {
+        private string typeOfTrain;
+        private int countOfWagon;       //Вагон на англ так и пишется 
+        private string typeOfLokomotive;
+
+    }
+}
